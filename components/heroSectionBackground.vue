81d599276f988841da53581f0840bab864ce44ec
MoltenDev <polkadavid@gmail.com>
2023-09-21 15:30:10 +0200

    deploy

--- components/heroSectionBackground.vue
+++ components/heroSectionBackground.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="bg-center bg-no-repeat bg-fixed bg-[url('/assets/images/heroBackground.jpg')]"
+    class="bg-center bg-cover bg-no-repeat bg-fixed bg-[url('/assets/images/heroBackground.jpg')]"
   ></div>
 </template>
 
---
+++ components/sectionOne.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <p>Aqui hi ha espai per posar una mica de presentació o alguna cosa així</p>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped></style>

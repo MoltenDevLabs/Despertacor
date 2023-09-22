e004e3c0a74be66a7ea5665db4268c7ae06fa00f
MoltenDev <polkadavid@gmail.com>
2023-09-23 00:18:30 +0200

    deploy

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -79,6 +79,8 @@ export default defineNuxtConfig({
   pages: true,
   supabase: {
     redirect: false,
+    url: process.env.SUPABASE_URL,
+    key: process.env.SUPABASE_KEY,
   },
   runtimeConfig: {
     public: {

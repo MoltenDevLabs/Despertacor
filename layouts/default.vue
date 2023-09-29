78bc8fa40edd6ba5436042b82a574120f0a87a42
MoltenDev <polkadavid@gmail.com>
2023-09-29 22:36:16 +0200

    navbar moved to bottom on mobile

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,58 +1,84 @@
 <template>
-  <div>
+  <!-- MOBILE START -->
+  <div v-if="$device.isMobileOrTablet">
     <!-- HEADER START -->
-    <div v-if="$device.isMobileOrTablet">
-      <header class="mx-auto px-[10%] flex items-center justify-between">
-        <div class="flex gap-2 items-center">
-          <nuxt-link to="/" @click="scrollToTop()">
-            <nuxt-icon :name="'logoS'" filled />
-          </nuxt-link>
-          <h5>Coral MusSolidària</h5>
-        </div>
-        <ul>
-          <button
-            class="btn p-1 border border-surface-900 dark:border-surface-200 flex flex-col justify-center items-center group"
-            @click="openMenu()"
-          >
-            <div
-              :class="`${hamburgerMenuLine} ${
-                navMenu
-                  ? 'rotate-45 translate-y-1.5 group-hover:opacity-100'
-                  : 'group-hover:opacity-100'
-              }`"
-            ></div>
-            <div
-              :class="`${hamburgerMenuLine} ${
-                navMenu ? 'opacity-0' : 'group-hover:opacity-100'
-              }`"
-            ></div>
-            <div
-              :class="`${hamburgerMenuLine} ${
-                navMenu
-                  ? '-rotate-45 -translate-y-1.5 group-hover:opacity-100'
-                  : 'group-hover:opacity-100'
-              }`"
-            ></div>
-          </button>
-        </ul>
-      </header>
-      <header
-        class="absolute top-[68px] min-w-full h-0 z-10 overflow-hidden transition-all ease-out duration-500"
-        :class="navMenu ? 'h-80 opacity-90' : 'h-0'"
+    <!-- HEADER END -->
+
+    <!-- CONTENT START -->
+    <main class="mb-16">
+      <slot />
+    </main>
+    <!-- CONTENT END -->
+
+    <!-- FOOTER START -->
+    <!--     <footer>
+      <div class="absolute inset-x-0 bottom-0 flex justify-center border-t">
+        <p class="text-xs tracking-wide mt-2 opacity-60">
+          Made with<span
+            class="material-icons-outlined align-text-bottom scale-[.8]"
+            >local_fire_department</span
+          >by MoltenDevLabs ©
+        </p>
+      </div>
+    </footer> -->
+    <footer
+      class="fixed bottom-0 mx-auto px-[4%] py-[4%] grid grid-cols-5 auto-cols-auto gap-2 justify-items-center border-t-2 border-surface-600 dark:border-surface-300"
+    >
+      <nuxt-link to="/" class="material-icons-outlined" @click="scrollToTop()"
+        >home</nuxt-link
       >
-        <ul class="flex flex-col items-center p-0 uppercase tracking-widest">
-          <button
-            class="absolute top-3 mx-[10%] flex place-self-end"
-            @click="themeStore.toggleTheme()"
-          >
-            <i
-              class="material-icons-outlined theme-icon scale-75"
-              :class="{ 'border-surface-900': !themeStore.isDarkTheme }"
-              :title="themeStore.isDarkTheme ? 'Light mode' : 'Dark mode'"
-            >
-              {{ themeStore.isDarkTheme ? "light_mode" : "dark_mode" }}
-            </i>
-          </button>
+      <nuxt-link
+        to="/artistes"
+        class="material-icons-outlined"
+        @click="scrollToTop()"
+        >diversity_1</nuxt-link
+      >
+      <nuxt-link
+        to="/temes"
+        class="material-icons-outlined"
+        @click="scrollToTop()"
+        >lyrics</nuxt-link
+      >
+      <nuxt-link
+        to="/concerts"
+        class="material-icons-outlined"
+        @click="scrollToTop()"
+        >photo_library</nuxt-link
+      >
+      <nuxt-link
+        :to="user ? '/profile' : '/auth'"
+        @click="scrollToTop()"
+        class="material-icons-outlined"
+      >
+        {{ user ? "account_circle" : "login" }}
+      </nuxt-link>
+      <!--         <div class="absolute inset-x-0 bottom-0 flex justify-end">
+          <p class="text-xs tracking-wide m-4 opacity-60">
+            Made with<span
+              class="material-icons-outlined align-text-bottom scale-[.8]"
+              >local_fire_department</span
+            >by MoltenDevLabs ©
+          </p>
+        </div> -->
+    </footer>
+    <!-- FOOTER END -->
+  </div>
+  <!-- MOBILE START -->
+
+  <!-- DESKTOP START -->
+  <div v-else>
+    <!-- HEADER START -->
+    <header
+      class="header mx-auto px-[10%] py-[2%] flex items-center justify-between"
+    >
+      <div class="flex gap-2 items-center">
+        <nuxt-link to="/" @click="scrollToTop()">
+          <nuxt-icon :name="'logoS'" title="Home" filled />
+        </nuxt-link>
+        <h5>Coral MusSolidària</h5>
+      </div>
+      <nav class="flex gap-4 items-center">
+        <ul class="flex gap-12 text-lg uppercase tracking-widest my-4">
           <li><nuxt-link to="/" @click="scrollToTop()">HOME</nuxt-link></li>
           <li>
             <nuxt-link to="/temes" @click="scrollToTop()">CANÇONS</nuxt-link>
@@ -64,58 +90,27 @@
           </li>
           <li>
             <nuxt-link to="/concerts" @click="scrollToTop()"
-              >Concerts</nuxt-link
+              >CONCERTS</nuxt-link
             >
           </li>
         </ul>
-      </header>
-    </div>
-
-    <div v-else>
-      <header
-        class="header mx-auto px-[10%] py-[2%] flex items-center justify-between"
-      >
-        <div class="flex gap-2 items-center">
-          <nuxt-link to="/" @click="scrollToTop()">
-            <nuxt-icon :name="'logoS'" title="Home" filled />
-          </nuxt-link>
-          <h5>Coral MusSolidària</h5>
-        </div>
-        <nav class="flex gap-4 items-center">
-          <ul class="flex gap-12 text-lg uppercase tracking-widest my-4">
-            <li><nuxt-link to="/" @click="scrollToTop()">HOME</nuxt-link></li>
-            <li>
-              <nuxt-link to="/temes" @click="scrollToTop()">CANÇONS</nuxt-link>
-            </li>
-            <li>
-              <nuxt-link to="/artistes" @click="scrollToTop()"
-                >ARTISTES</nuxt-link
-              >
-            </li>
-            <li>
-              <nuxt-link to="/concerts" @click="scrollToTop()"
-                >CONCERTS</nuxt-link
-              >
-            </li>
-          </ul>
-        </nav>
-        <nav class="flex items-center">
-          <button class="flex" @click="themeStore.toggleTheme()">
-            <i
-              class="material-icons-outlined theme-icon mx-8 scale-90"
-              :class="{ 'border-surface-900': !themeStore.isDarkTheme }"
-              :title="themeStore.isDarkTheme ? 'Light mode' : 'Dark mode'"
-            >
-              {{ themeStore.isDarkTheme ? "light_mode" : "dark_mode" }}
-            </i>
-          </button>
-        </nav>
-      </header>
-    </div>
+      </nav>
+      <nav class="flex items-center">
+        <button class="flex" @click="themeStore.toggleTheme()">
+          <i
+            class="material-icons-outlined theme-icon mx-8 scale-90"
+            :class="{ 'border-surface-900': !themeStore.isDarkTheme }"
+            :title="themeStore.isDarkTheme ? 'Light mode' : 'Dark mode'"
+          >
+            {{ themeStore.isDarkTheme ? "light_mode" : "dark_mode" }}
+          </i>
+        </button>
+      </nav>
+    </header>
     <!-- HEADER END -->
 
     <!-- CONTENT START -->
-    <main :class="navMenu ? 'blur-sm' : ''">
+    <main>
       <slot />
     </main>
     <!-- CONTENT END -->
@@ -170,26 +165,18 @@
     </div>
     <!-- FOOTER END -->
   </div>
+  <!-- DESKTOP END -->
 </template>
 
 <script setup>
-const hamburgerMenuLine =
-  "h-0.5 w-5 my-0.5 rounded-full bg-surface-900 dark:bg-surface-200 transition ease transform duration-500";
-
 import { useThemeStore } from "@/stores/themeStore";
 const themeStore = useThemeStore();
-
-const navMenu = ref(false);
-const openMenu = () => {
-  navMenu.value = !navMenu.value;
-};
 
 const scrollToTop = () => {
   window.scrollTo({
     top: 0,
     behavior: "smooth",
   });
-  navMenu.value = false;
 };
 </script>
 

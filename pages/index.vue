8b981098be2a2ebb8bd93394fe693512ad09f8c6
MoltenDev <polkadavid@gmail.com>
2023-09-29 13:51:20 +0200

    small changes

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <heroSectionBackground
-      class="opacity-80 h-[34rem] absolute top-0 -mx-[6%] min-w-full"
+      class="h-[34rem] absolute top-0 -mx-[6%] min-w-full"
     />
     <heroSection class="relative mb-40 md:mb-44 mt-20 md:mt-12" />
     <sectionOne class="my-24" />
